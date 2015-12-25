ff5a581e3877f0b8527ef7ca3044ea12faef66c4
sunil <sunilkumar1117@gmail.com>
2015-12-24 17:08:09 -0800

    Maven Jenkin Test project cretaed

---
+++ maven-jenkintest/src/main/java/com/sunil/webdev/App.java
@@ -0,0 +1,13 @@
+package com.sunil.webdev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
